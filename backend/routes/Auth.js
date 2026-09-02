123c0990b680d9c022ca5732b174bd75e4bd40f0
unknown <mahechamanuela92@gmail.com>
2026-09-02 09:26:38 -0500

    subiendo cambios rutas y servicios

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { registro, login } from "../controllers/Auth.js";
+import { registro, login, verificarCuenta } from "../controllers/Auth.js";
 import { olvidarPassword, verifyCode } from "../controllers/recuperar.js";
 
 const router = express.Router();
 
+//rutas de autenticacion 
 router.post("/register", registro);
 router.post("/login", login);
+router.post ('/verify-account', verificarCuenta);
 
 // Rutas de recuperación de contraseña
 router.post("/forgot-password", olvidarPassword);
